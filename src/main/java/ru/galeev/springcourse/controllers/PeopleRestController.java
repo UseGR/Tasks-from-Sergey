acd4864356ed5bf69dd16f60daf0cf4fcc80a875
deadm <deadmorozz50@gmail.com>
2022-08-15 22:58:22 +0300

    My first REST application + slf4j logger

--- src/main/java/ru/galeev/springcourse/controllers/PeopleRestController.java
+++ src/main/java/ru/galeev/springcourse/controllers/PeopleRestController.java
@@ -1,5 +1,7 @@
 package ru.galeev.springcourse.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class PeopleRestController {
     private final PeopleService peopleService;
+    private static final Logger LOG = LoggerFactory.getLogger(PeopleRestController.class);
 
     @Autowired
     public PeopleRestController(PeopleService peopleService) {
@@ -27,11 +30,13 @@ public class PeopleRestController {
 
     @GetMapping
     public List<Person> getPeople() {
+        LOG.warn("Getting all people");
         return peopleService.findAll();
     }
 
     @GetMapping("/{id}")
     public Person getPerson(@PathVariable("id") int id) {
+        LOG.warn("Getting person by ID");
         return peopleService.findOne(id);
     }
 
@@ -47,10 +52,38 @@ public class PeopleRestController {
 
             throw new PersonNotCreatedException(errorMessage.toString());
         }
-
+        LOG.warn("Insert new person in DB");
         peopleService.save(person);
+        LOG.warn("New person successfully inserted");
 
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<HttpStatus> update(@RequestBody @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder errorMessage = new StringBuilder();
+
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError error : errors) {
+                errorMessage.append(error.getField()).append(" - ").append(error.getDefaultMessage()).append(";\n");
+            }
+
+            throw new PersonNotCreatedException(errorMessage.toString());
+        }
+        LOG.warn("Updating person in DB");
+        peopleService.save(person);
+        LOG.warn("Data is updated");
+
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable int id) {
+        Person person = getPerson(id);
+        peopleService.delete(id);
+        LOG.warn("Person was removed");
+        return "Person was removed";
     }
 
     @ExceptionHandler
@@ -59,7 +92,7 @@ public class PeopleRestController {
                 "Person isn't found!",
                 System.currentTimeMillis()
         );
-
+        LOG.error("Error! Person isn't found");
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
@@ -69,7 +102,7 @@ public class PeopleRestController {
                 e.getMessage(),
                 System.currentTimeMillis()
         );
-
+        LOG.error("Bad request");
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/ru/galeev/springcourse/services/PeopleService.java
+++ src/main/java/ru/galeev/springcourse/services/PeopleService.java
@@ -1,8 +1,11 @@
 package ru.galeev.springcourse.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.galeev.springcourse.controllers.PeopleRestController;
 import ru.galeev.springcourse.models.Person;
 import ru.galeev.springcourse.repositories.PeopleRepository;
 import ru.galeev.springcourse.util.PersonNotFoundException;
@@ -13,6 +16,7 @@ import java.util.Optional;
 @Service
 @Transactional(readOnly = true)
 public class PeopleService {
+    private static final Logger LOG = LoggerFactory.getLogger(PeopleService.class);
     private final PeopleRepository peopleRepository;
 
     @Autowired
@@ -21,27 +25,32 @@ public class PeopleService {
     }
 
     public List<Person> findAll() {
+        LOG.info("Method findAll returns all entities");
         return peopleRepository.findAll(); //.findAll (возвращает все сущности из БД)
     }
 
     public Person findOne(int id) {
         Optional<Person> foundPerson = peopleRepository.findById(id);
+        LOG.info("Method findOne returns entity by ID");
         return foundPerson.orElseThrow(PersonNotFoundException::new);
     }
 
     @Transactional
     public void save(Person person) {
         peopleRepository.save(person);
+        LOG.info("Method save keeps entity");
     }
 
     @Transactional
     public void update(int id, Person updatedPerson) {
         updatedPerson.setId(id);
         peopleRepository.save(updatedPerson);
+        LOG.info("Method update updates entity by ID");
     }
 
     @Transactional
     public void delete(int id) {
         peopleRepository.deleteById(id);
+        LOG.info("Method delete removes entity by ID");
     }
 }
